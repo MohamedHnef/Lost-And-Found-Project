0ed58a82b38cea0f6ea633f505dd6a3ee628ae78
MohamedHanifProg <signutre.31@gmail.com>
2024-06-28 21:54:39 +0300

    render

--- server/server.js
+++ server/server.js
@@ -31,9 +31,8 @@ app.use(cors({
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-
-app.use(express.static(path.join(__dirname, '.')));
-
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve uploads directory
 const uploadsDir = path.join(__dirname, 'server', 'uploads');
