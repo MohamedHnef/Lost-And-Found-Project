b8fa903bff24a56b371d53e0f4b66859e3296f32
Amir Khalifa <amir.kh28@hotmail.com>
2024-06-28 18:16:38 +0300

    Debugging deployment on render

--- server/db.js
+++ server/db.js
@@ -1,12 +1,13 @@
 // db.js
 const mysql = require('mysql');
+require('dotenv').config();
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: '148.66.138.145',
-  user: 'dbusrShnkr24',
-  password: 'studDBpwWeb2!',
-  database: 'dbShnkr24stud'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 pool.getConnection((err, connection) => {
