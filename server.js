49a0f0ad0313c44816a9d97a1ef7468e7f646a8f
Amir Khalifa <amir.kh28@hotmail.com>
2024-06-28 18:36:35 +0300

    Add route handler for root URL to serve index.html

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const routes = require('./server/routes');
+const routes = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,6 +11,11 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use((req, res, next) => {
+    res.setHeader("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'");
+    next();
+});
+
 
 // Dynamic CORS setup
 app.use(cors({
@@ -24,7 +29,7 @@ app.use(cors({
 }));
 
 // Serve static files
-app.use(express.static(path.join(__dirname, '..', 'public'))); // Ensure this path is correct
+app.use(express.static(path.join(__dirname))); // Serve static files from the root directory
 
 // Serve uploaded files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
@@ -32,6 +37,10 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // API routes
 app.use('/api', routes);
 
+// Serve index.html for the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
